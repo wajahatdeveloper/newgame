ba0a48ab27799b4095d6014166ef96ab89414a9d
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2021-10-09 19:30:40 +0500

    Added Waypoints System, New Board and Rigged the new board with the waypoint system

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WaypointsFree;
+
+public class Hero : MonoBehaviour
+{
+	public SpriteRenderer spriteRenderer;
+	public WaypointsTraveler waypointsTraveler;
+}
